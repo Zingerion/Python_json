d72ecafa170973f82fef72937e8cc0967270b9b9
Daniil Emelianov <zingerion@gmail.com>
2021-01-30 14:06:54 +0300

    Скласс UserUnfo

--- Main.py
+++ Main.py
@@ -4,8 +4,12 @@ import datetime
 import datetime
 from datetime import date
 
-today = datetime.datetime.today()
-print( today.strftime("%d.%m.%Y %H:%M") )
+# var = ['www.pythonru', 'com']
+# print ('\n'.join(var))
+# today = datetime.datetime.today()
+# print( today.strftime("%d.%m.%Y %H:%M") )
+
+
 #url ='https://json.medrating.org/todos'
 #r = requests.get(url)
 #data = r.json()
--- venv/UserInfo.py
+++ venv/UserInfo.py
@@ -8,34 +8,51 @@ class UserInfo:
         self.compl_amount = compl_amount
         self.compl_tasks = compl_tasks
         self.remain_amount = remain_amount
-        self.remain_tasks = remain_tasks0
+        self.remain_tasks = remain_tasks
+        self.make_top()
+        self.make_second()
+        self.make_total_amount()
+        self.make_compl_amount()
+        self.make_compl_list
+        self.make_ramain_amount
+        self.make_ramain_list()
         self.make_file()
+        self.save_txt()
 
     def make_top(self):
-        time_now =
         first_string = "Отчет" + ' ' + company + '.' + '\n'
         return first_string
 
     def make_second(self):
-        second_string = name + ' ' + '<' + mail + '>' +
+        today = datetime.datetime.today()
+        proper_format_today = today.strftime("%d.%m.%Y %H:%M")
+        second_string = name + ' ' + '<' + mail + '>' + proper_format_today + '\n'
+        return second_string
 
     def make_total_amount(self):
-        pass
+        total_amount = "Всего задач: " + amount + '\n\n'
+        return total_amount
 
     def make_compl_amount(self):
-        pass
+        compl_amount_str = "Завершенные задачи: " + '(' + compl_amount  + '):' + '\n'
+        return compl_amount_str
 
     def make_compl_list(self):
-        pass
+        compl_tasks_list = ('\n'.join(compl_tasks))
+        return compl_tasks_list
 
     def make_ramain_amount(self):
-        pass
+        remain_amount_str = "Оставшиеся задачи: " + '(' + remain_amount + '):' + '\n'
+        return remain_amount_str
 
     def make_ramain_list(self):
-        pass
+        remain_tasks_list = ('\n'.join(remain_tasks))
+        return remain_tasks_list
 
     def make_file(self):
-        pass
+        filed_file = make_top() + make_second() + make_total_amount() + make_compl_amount() + make_compl_list() + make_ramain_amount() + make_ramain_list()
 
     def save_txt(self):
-        pass
+        my_file = open(name + ".txt", "w")
+        my_file.write(make_file())
+        my_file.close()
