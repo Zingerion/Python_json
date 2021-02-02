eff1f4b981748fe339b6e9cc47451ed1717b8a5e
Daniil Emelianov <zingerion@gmail.com>
2021-02-02 19:49:16 +0300

    Сохранение всех заданий

--- Main.py
+++ Main.py
@@ -20,40 +20,50 @@ todos = json.loads(todos_json.text)
 users_json = requests.get("https://json.medrating.org/users")
 users = json.loads(users_json.text)
 
-all_users_info = []
-
+all_users_info = {}
 
 def get_all_tasks(todos, users):
-    total_task_amount = {}
+
     for user in users:
-        individual_tasks = []
+        completed_task_list = []
+        remain_task_list = []
+        total_task_amount = []
         for todo in todos:
             try:
                 if todo["userId"] == user["id"]:
-                    individual_tasks.append(todo["title"])
+                    if todo["completed"]:
+                        completed_task_list.append(todo["title"])
+                        # pprint.pprint(completed_task_list)
+                    else:
+                        remain_task_list.append(todo["title"])
             except KeyError:
                 pass
-        total_task_amount[user["id"]] = individual_tasks
-    return total_task_amount
+        all_users_info[user["id"]] = {}
+        all_users_info[user["id"]]["amount"] = len(completed_task_list) + len(remain_task_list)
+        all_users_info[user["id"]]["compl_tasks"] = completed_task_list
+        all_users_info[user["id"]]["compl_amount"] = len(completed_task_list)
+        all_users_info[user["id"]]["remain_tasks"] = remain_task_list
+        all_users_info[user["id"]]["remain_amount"] = len(remain_task_list)
+    pprint.pprint(all_users_info)
 
-
-# pprint.pprint(get_all_tasks(todos, users))
 
 def total_amount_task(taskt_list):
     for key in taskt_list:
         task_amount_per_user = len(taskt_list[key])
-        all_users_info.append({key : {"amount": task_amount_per_user}})
+        all_users_info.append({key: {"amount": task_amount_per_user}})
     return all_users_info
 
-def compl_amount_tasks(taskt_list):
-    for key in taskt_list:
-        kk
 
-xx = get_all_tasks(todos, users)
+# def compl_amount_tasks(taskt_list):
+#     for key in taskt_list:
+#         kk
+# pprint.pprint(get_all_tasks(todos, users))
+get_all_tasks(todos, users)
+# xx = get_all_tasks(todos, users)
 
 # pprint.pprint((xx))
 # pprint.pprint(total_amount_task(xx))
-total_amount_task(xx)
+# pprint.pprint(total_amount_task(xx))
 
 
 def test_func(a, b, c):
